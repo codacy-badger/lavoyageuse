57cc88a811bc394ab8bc108bcf445c8c90c2954c
Maxime BOué <abriko@yahoo.fr>
2018-09-14 11:56:37 +0200

    Start registration screen

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914084541) do
+ActiveRecord::Schema.define(version: 20180914091009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "host"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.integer "host"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "host"
+    t.string "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +54,6 @@ ActiveRecord::Schema.define(version: 20180914084541) do
     t.index ["{:foreign_key=>true}_id"], name: "index_users_on_{:foreign_key=>true}_id"
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "trips", "users"
 end
