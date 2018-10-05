8fa162a48f8818a68375fc5bec5f32bfa75bc42d
Maxime BOué <abriko@yahoo.fr>
2018-10-05 11:32:17 +0200

    Fix seed and add trip creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.destroy_all
+Trip.destroy_all
 
 emails = %w(sarahfarrel.b@gmail.com faith.R@gmail.com june.hullard@yahoo.fr Celeste.in@gmail.com Steph.croods@girst.com Marge@simpson.com Shan@shen.cn helen.Christobale@world.co judith@hero.com)
 password = "LVa123456"
@@ -20,44 +22,57 @@ addresses = [ "22 Rue de la Preceinte, Le Bouscat",
             "9 Rue André Darbon, Bordeaux",
             "124 Rue Lucien Faure, Bordeaux",
             "65 Quai des Chartrons, Bordeaux"]
-role = 1
-sentences = ["J'aime le yoga et la cuisine,
-              J'aime l'art et la danse,
-              J'aime voyager et faire des rencontres,
-              J'aimes les éléphants bleus et les chats loufoques,
-              J'aime rire entre amis,
-              J'aime me dire qu'il est bon de changer,
-              Je serai votre meilleur hôte à Bordeaux,
-              J'adore les sushis et les films de ninja,
-              J'aime voir le monde et ses couleurs"]
-photos = ["https://res.cloudinary.com/dfcsmghw4/image/upload/v1537346964/seed/Teenager-dies-after-rolling-onto-broken-iPhone-cable-in-her-sleep.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347615/seed/photo-1458662236860-b721a6735660.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347595/seed/photo-1463763422025-e07f8081b9c9.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347576/seed/photo-1493666438817-866a91353ca9.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347489/seed/screen-shot-2018-09-05-at-5-27-46-pm-e1536186712841.png",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347401/seed/woman.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347355/seed/_95182277_jesprofile3.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347211/seed/_6cfb1a60-a9ed-11e8-abd2-5c322fa89f61.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347055/seed/hanan-1.jpg",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537347006/seed/lilliya-scarlett-instagram-1528814125-custom-0.png",
-          "https://res.cloudinary.com/dfcsmghw4/image/upload/v1537346964/seed/Teenager-dies-after-rolling-onto-broken-iPhone-cable-in-her-sleep.jpg"]
+sentences = ["J'aime le yoga et la cuisine",
+              "J'aime l'art et la danse",
+              "J'aime voyager et faire des rencontres",
+              "J'aimes les éléphants bleus et les chats loufoques",
+              "J'aime rire entre amis",
+              "J'aime me dire qu'il est bon de changer",
+              "Je serai votre meilleur hôte à Bordeaux",
+              "J'adore les sushis et les films de ninja",
+              "J'aime voir le monde et ses couleurs"]
+photos = ["https://res.cloudinary.com/dfcsmghw4/image/private/s--ZCdIf1Ex--/v1537430205/seed/csrcm4ulusejyngjdzhn.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--23Jzortx--/v1537430206/seed/ylhs5obe9bgts6bft4gl.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--vgs7jyN7--/v1537430208/seed/xf0gn4fvwr18f1oyqtfg.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--JGrNKbnx--/v1537430209/seed/e9uk2gmry3yo3wt00ggx.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--PEVNC-cD--/v1537430211/seed/kub6ozsw9ycrktmyov3c.png",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--H8FC5tZV--/v1537430212/seed/hp9dcodxaecttocu3fso.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s---au51Qjz--/v1537430213/seed/vgfklhwblyyt5lqm83rt.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--5NQ5mEFZ--/v1537430215/seed/rjmrbr7a6hrfafwsjsyq.jpg",
+          "https://res.cloudinary.com/dfcsmghw4/image/private/s--yS2F3HeI--/v1537430216/seed/rmscx1xilaysbhbm4dts.jpg"]
 birth_dates = rand(18..40).to_s
+dates = ["01/04/2019", "13/12/2018","01/11/2018","22/10/2018","16/12/20018","21/11/2018","22/01/2019","01/01/2019","02/02/2019","05/05/2019"]
 
 
-emails.each_with_index do |x, i|
-  puts "Creating #{first_names[i]} ..."
-  User.create!(email: x,
-               password: password,
-               first_name: first_names[i],
-               last_name: last_names[i],
-               phone: phone,
-               sentences: sentences[i],
-               address: addresses[i],
-               role: role,
-               birth_date: "10/05/1995",
-               photo: open(photos[i]))
-  print "> #{first_names[i]} created !"
+# emails.each_with_index do |x, i|
+#   print "Creating #{first_names[i]} ..."
+#   User.create!(email: x,
+#                password: password,
+#                first_name: first_names[i],
+#                last_name: last_names[i],
+#                phone: phone,
+#                sentence: sentences[i],
+#                address: addresses[i],
+#                role: rand(1..2),
+#                host: rand(0..2),
+#                birth_date: "10/05/1995",
+#                photo: open(photos[i]))
+
+#   puts "> #{first_names[i]} created !"
+# end
+
+puts "Creating trips"
+print "["
+10.times do |index|
+  user = host = User.find(rand(User.first.id..User.last.id))
+  while host == user
+    host = User.find(rand(User.first.id..User.last.id))
+  end
+  Trip.create!(user:user, host: host.id , date: dates[index])
+  print "-"
 end
+puts ["]"]
+puts "Trips created"
 
 # User.create!(email: "sarah7.b@gmail.com",
 #              password: "LVa123456" ,
