7feb931d4badd04b140edd6eb32e729a82ac588e
Maxime BOué <abriko@yahoo.fr>
2018-10-15 21:31:13 +0200

    Working on trip#create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,15 +1,27 @@
 class TripsController < ApplicationController
-  before_action :user_params, only: %I(new create)
+  before_action :params_user, only: %I(new create)
 
   def new
   end
 
   def create
+    @trip = Trip.new(traveller: current_user, host: @user, beginning: trips_params[:beginning], end: trips_params[:end])
+    if @trip.save!
+      redirect_to @user
+      flash[:success] = "success"
+    else
+      flash[:warning] = "Successfully created..."
+      redirect_to new_user_trip_path(@user)
+    end
   end
 
   private
 
-  def user_params
+  def params_user
     @user = User.find(params[:user_id])
+  end
+
+  def trips_params
+    params.require(:trip).permit(:beginning, :end)
   end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
   validates_presence_of :date
-  belongs_to :traveller
-  belongs_to :host
+  belongs_to :traveller, class_name: "User"
+  belongs_to :host, class_name: "User"
 end
