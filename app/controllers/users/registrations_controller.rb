c1bd465d9da2d1426bd381f3c4fe565b0a12ee7a
Maxime BOué <abriko@yahoo.fr>
2018-12-04 12:36:46 +0100

    Redirect to edit page after update with flash notice internationalizec

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,8 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_update_path_for(resource)
-      puts 'this is happening yoyo mama'
-      flash[:notice] = "Account succesfully updated"
-      edit_user_registration_path
+    flash[:success] = t('.account_updated_successfully')
+    edit_user_registration_path
   end
 end
